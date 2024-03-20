871424eb222a63934bdfa25e3f5d41e0e2113138
ThuyDoan0112 <doanthuy896@gmail.com>
2024-03-20 17:21:43 +0700

    :lipstick: Implement default layout

---
+++ layouts/default.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const title = 'HaUI Project Tracking'
+
+const links = []
+
+const groups = [
+  {
+    key: 'links',
+    label: 'Go to',
+    commands: links.map(link => ({
+      ...link,
+      shortcuts: link.tooltip?.shortcuts,
+    })),
+  },
+]
+</script>
+
+<template>
+  <UDashboardLayout>
+    <UDashboardPanel
+      :width="250"
+      :resizable="{ min: 200, max: 300 }"
+      collapsible
+    >
+      <UDashboardNavbar class="!border-transparent" :ui="{ left: 'flex-1' }">
+        <template #left>
+          <h1 class="text-xl font-bold text-primary">
+            {{ title }}
+          </h1>
+        </template>
+      </UDashboardNavbar>
+
+      <UDivider class="sticky top-0" />
+
+      <UDashboardSidebar>
+        <template #header>
+          <UDashboardSearchButton />
+        </template>
+
+        <UDashboardSidebarLinks :links="links" />
+      </UDashboardSidebar>
+    </UDashboardPanel>
+
+    <slot />
+
+    <ClientOnly>
+      <LazyUDashboardSearch :groups="groups" />
+    </ClientOnly>
+  </UDashboardLayout>
+</template>
