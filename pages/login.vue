23c6f02758723df32ea33560e7dfa2b3b601b5f0
ThuyDoan0112 <doanthuy896@gmail.com>
2024-04-07 10:20:10 +0700

    :lipstick: Implement login page

---
+++ pages/login.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'auth',
+})
+
+useSeoMeta({
+  title: 'Login',
+})
+
+const fields = [
+  {
+    name: 'email',
+    type: 'text',
+    label: 'Email',
+    placeholder: 'Enter your email',
+  },
+  {
+    name: 'password',
+    label: 'Password',
+    type: 'password',
+    placeholder: 'Enter your password',
+  },
+]
+
+function validate(state: any) {
+  const errors = []
+  if (!state.email)
+    errors.push({ path: 'email', message: 'Email is required' })
+  if (!state.password)
+    errors.push({ path: 'password', message: 'Password is required' })
+  return errors
+}
+
+function handleLogin(data: any) {
+  console.log('Submitted', data)
+}
+</script>
+
+<template>
+  <UCard class="max-w-sm w-full bg-white/75 dark:bg-white/5 backdrop-blur">
+    <UAuthForm
+      :fields="fields"
+      :validate="validate"
+      title="Welcome back"
+      align="top"
+      icon="i-heroicons-lock-closed"
+      :ui="{ base: 'text-center', footer: 'text-center' }"
+      :submit-button="{ trailingIcon: 'i-heroicons-arrow-right-20-solid' }"
+      @submit="handleLogin"
+    />
+  </UCard>
+</template>
