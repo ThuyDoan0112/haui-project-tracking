3e163664e39c5ab1cb96b313e1f84f0c4f2fc9d8
ThuyDoan0112 <doanthuy896@gmail.com>
2024-05-01 10:57:13 +0700

    :fire: Remove switch colors theme

--- layouts/default.vue
+++ layouts/default.vue
@@ -62,21 +62,6 @@ const groups = computed(() => {
     },
   ]
 })
-
-const appConfig = useAppConfig()
-const defaultColors = ref(
-  ['green', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet'].map(color => ({
-    label: color,
-    chip: color,
-    click: () => (appConfig.ui.primary = color),
-  })),
-)
-const colors = computed(() =>
-  defaultColors.value.map(color => ({
-    ...color,
-    active: appConfig.ui.primary === color.label,
-  })),
-)
 </script>
 
 <template>
@@ -100,13 +85,6 @@ const colors = computed(() =>
         </template>
 
         <UDashboardSidebarLinks :links="links" />
-
-        <UDivider />
-
-        <UDashboardSidebarLinks
-          :links="[{ label: 'Colors', draggable: true, children: colors }]"
-          @update:links="(colors) => (defaultColors = colors)"
-        />
 
         <UDivider class="sticky bottom-0" />
 
