2722febc30a55034b224e395245e3dccd53d343a
ThuyDoan0112 <doanthuy896@gmail.com>
2024-05-01 17:08:10 +0700

    :hammer: Update tasks form UI

--- components/tasks/TasksForm.vue
+++ components/tasks/TasksForm.vue
@@ -6,7 +6,6 @@ defineProps<{
 }>()
 
 const emit = defineEmits<{
-  close: []
   submit: [data: any]
 }>()
 
@@ -21,14 +20,14 @@ function validate(state: any): FormError[] {
   if (!state.name)
     errors.push({ path: 'name', message: 'Please enter a name.' })
 
-  if (!state.description)
-    errors.push({ path: 'description', message: 'Please enter a description.' })
-
   return errors
 }
 
 function onSubmit(event: FormSubmitEvent<any>) {
   emit('submit', event.data)
+
+  // Reset the form
+  state.name = undefined
 }
 </script>
 
@@ -37,34 +36,20 @@ function onSubmit(event: FormSubmitEvent<any>) {
     :validate="validate"
     :validate-on="['submit']"
     :state="state"
-    class="space-y-4"
+    class="space-y-4 flex items-end gap-4"
     @submit="onSubmit"
   >
-    <UFormGroup label="Name" name="name">
+    <UFormGroup label="New Task" name="name" class="w-1/2">
       <UInput
         v-model="state.name"
-        placeholder="Enter task name"
         autofocus
       />
     </UFormGroup>
 
-    <UFormGroup label="Description" name="description">
-      <UTextarea
-        v-model="state.description"
-        type="text"
-        placeholder="Enter task description"
-        rows="10"
-      />
-    </UFormGroup>
-
-    <div class="flex justify-end gap-3">
-      <UButton
-        label="Cancel"
-        color="gray"
-        variant="ghost"
-        @click="emit('close')"
-      />
-      <UButton :loading="loading" type="submit" label="Save" color="black" />
-    </div>
+    <UButton
+      label="Save"
+      icon="i-heroicons-plus"
+      type="submit"
+    />
   </UForm>
 </template>
